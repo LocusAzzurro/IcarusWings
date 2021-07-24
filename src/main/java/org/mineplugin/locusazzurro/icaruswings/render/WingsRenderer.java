8f7d40bf2ac3814cc1f688850a5979006dc156e6
LocusAzzurro <dingzx1997@gmail.com>
2021-07-24 10:04:14 +0200

    add render layer to all entities

--- src/main/java/org/mineplugin/locusazzurro/icaruswings/render/WingsRenderer.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/render/WingsRenderer.java
@@ -1,9 +1,17 @@
 package org.mineplugin.locusazzurro.icaruswings.render;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.client.renderer.entity.PlayerRenderer;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,6 +22,7 @@ import net.minecraftforge.fml.common.Mod;
 public class WingsRenderer {
 	
 	private static boolean init = false;
+	private static List<Entity> entityList = new ArrayList<>();
 	
 	@OnlyIn(Dist.CLIENT)
 	@SubscribeEvent (priority = EventPriority.LOW)
@@ -24,5 +33,18 @@ public class WingsRenderer {
 		init = true;
 		}
 
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent (priority = EventPriority.LOW)
+	public static void onRenderEntity(RenderLivingEvent<?, ?> event) {
+		LivingRenderer<? extends LivingEntity, ? extends EntityModel<?>> renderer = event.getRenderer();
+		Entity entity = event.getEntity();
+		if(!entityList.contains(entity)) {
+			renderer.addLayer(new WingsLayer(renderer));
+			entityList.add(entity);
+		}
+		
 	}
 }
