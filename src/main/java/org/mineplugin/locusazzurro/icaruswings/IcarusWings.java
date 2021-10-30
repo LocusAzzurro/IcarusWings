cb63c73c82664eb86b7e47e40e3a4fb1d47dc1b0
LocusAzzurro <dingzx1997@gmail.com>
2021-10-30 17:20:23 +0200

    Adding 2 effects
    * Poison Immunity
    * Sensory Masking
    * Related reg and lang

--- src/main/java/org/mineplugin/locusazzurro/icaruswings/IcarusWings.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/IcarusWings.java
@@ -1,23 +1,29 @@
 package org.mineplugin.locusazzurro.icaruswings;
 
 import org.mineplugin.locusazzurro.icaruswings.data.BlockRegistry;
+import org.mineplugin.locusazzurro.icaruswings.data.EffectRegistry;
 import org.mineplugin.locusazzurro.icaruswings.data.EnchantmentRegistry;
+import org.mineplugin.locusazzurro.icaruswings.data.EntityTypeRegistry;
 import org.mineplugin.locusazzurro.icaruswings.data.ItemRegistry;
 import org.mineplugin.locusazzurro.icaruswings.data.SoundRegistry;
 import org.mineplugin.locusazzurro.icaruswings.data.TileEntityTypeRegistry;
 import org.mineplugin.locusazzurro.icaruswings.data.Utils;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Utils.MOD_ID)
 public class IcarusWings {
 	public IcarusWings() {
-	ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-	BlockRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-	TileEntityTypeRegistry.TILE_ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
-	SoundRegistry.SOUNDS.register(FMLJavaModLoadingContext.get().getModEventBus());
-	EnchantmentRegistry.ENCHANTMENTS.register(FMLJavaModLoadingContext.get().getModEventBus());
-	
+		
+	IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+	ItemRegistry.ITEMS.register(bus);
+	BlockRegistry.BLOCKS.register(bus);
+	TileEntityTypeRegistry.TILE_ENTITIES.register(bus);
+	SoundRegistry.SOUNDS.register(bus);
+	EnchantmentRegistry.ENCHANTMENTS.register(bus);
+	EffectRegistry.EFFECTS.register(bus);
+	EntityTypeRegistry.ENTITIES.register(bus);
 	}
 }
