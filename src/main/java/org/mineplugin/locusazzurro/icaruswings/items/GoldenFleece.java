f6a1e11d0ecb7ccacf8bb573d3848f419045dd3d
LocusAzzurro <dingzx1997@gmail.com>
2022-01-20 21:53:24 +0100

    add golden fleece item

---
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/items/GoldenFleece.java
@@ -0,0 +1,11 @@
+package org.mineplugin.locusazzurro.icaruswings.items;
+
+import net.minecraft.item.Item;
+import org.mineplugin.locusazzurro.icaruswings.data.ModGroup;
+
+public class GoldenFleece extends Item{
+
+    public GoldenFleece() {
+        super(new Item.Properties().tab(ModGroup.itemGroup));
+    }
+}
--- src/main/java/org/mineplugin/locusazzurro/icaruswings/registry/ItemRegistry.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/registry/ItemRegistry.java
@@ -99,6 +99,7 @@ public class ItemRegistry {
     public static final RegistryObject<Item> wheatGrains = ITEMS.register("wheat_grains", WheatGrains::new);
     public static final RegistryObject<Item> wheatString = ITEMS.register("wheat_string", WheatString::new);
     public static final RegistryObject<Item> goldenString = ITEMS.register("golden_string", GoldenString::new);
+    public static final RegistryObject<Item> goldenFleece = ITEMS.register("golden_fleece", GoldenString::new);
     
     public static final RegistryObject<Item> linen = ITEMS.register("linen", Linen::new);
     
