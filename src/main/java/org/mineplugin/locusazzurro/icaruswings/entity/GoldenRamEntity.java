1accf099f02256b8bc23600632e6ff8af441bcb3
morning <875899749@qq.com>
2022-01-22 16:36:59 +0800

    add GoldenRamEntity Sheared

--- src/main/java/org/mineplugin/locusazzurro/icaruswings/entity/GoldenRamEntity.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/entity/GoldenRamEntity.java
@@ -7,24 +7,37 @@ import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.PanicGoal;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.IPacket;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.IForgeShearable;
 import net.minecraftforge.fml.network.NetworkHooks;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class GoldenRamEntity extends AnimalEntity{
+import java.util.Collections;
+import java.util.List;
+
+public class GoldenRamEntity extends AnimalEntity implements IForgeShearable {
+
+	private static final DataParameter<Boolean> IS_SHEARED = EntityDataManager.defineId(GoldenRamEntity.class, DataSerializers.BOOLEAN);
 	
 	public GoldenRamEntity(EntityType<GoldenRamEntity> entityType, World worldIn) {
 		super(entityType, worldIn);
@@ -33,16 +46,19 @@ public class GoldenRamEntity extends AnimalEntity{
 	@Override
 	public void defineSynchedData() {
 		super.defineSynchedData();
+		this.entityData.define(IS_SHEARED, false);
 	}
   
 	@Override
 	public void readAdditionalSaveData(CompoundNBT compound) {
 		super.readAdditionalSaveData(compound);
+		this.setSheared(compound.getBoolean("Sheared"));
     }
 	
 	@Override
 	public void addAdditionalSaveData(CompoundNBT compound) {
 		super.addAdditionalSaveData(compound);
+		compound.putBoolean("Sheared", this.isSheared());
     }
 	
 	@Override
@@ -81,6 +97,40 @@ public class GoldenRamEntity extends AnimalEntity{
 	@Override
 	public AgeableEntity getBreedOffspring(ServerWorld p_241840_1_, AgeableEntity p_241840_2_) {
 		return null;
+	}
+
+	public void setSheared(boolean isSheared) {
+		this.entityData.set(IS_SHEARED, isSheared);
+	}
+
+	public boolean isSheared() {
+		return this.entityData.get(IS_SHEARED);
+	}
+
+	public void ate() {
+		this.setSheared(false);
+	}
+
+	@Override
+	public boolean isShearable(@Nonnull ItemStack item, World world, BlockPos pos) {
+		return this.isAlive() && !this.isSheared();
+	}
+
+	@Nonnull
+	@Override
+	public List<ItemStack> onSheared(@Nullable PlayerEntity player, @Nonnull ItemStack item, World world, BlockPos pos, int fortune) {
+		world.playSound(null, this, SoundEvents.SHEEP_SHEAR, player == null ? SoundCategory.BLOCKS : SoundCategory.PLAYERS, 1.0F, 1.0F);
+		if (!world.isClientSide) {
+			this.setSheared(true);
+			int i = 1 + this.random.nextInt(3);
+
+			java.util.List<ItemStack> items = new java.util.ArrayList<>();
+			for (int j = 0; j < i; ++j) {
+				items.add(new ItemStack(Items.GOLDEN_APPLE));
+			}
+			return items;
+		}
+		return Collections.emptyList();
 	}
 	
 
--- src/main/java/org/mineplugin/locusazzurro/icaruswings/render/GoldenRamFleeceLayer.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/render/GoldenRamFleeceLayer.java
@@ -21,7 +21,7 @@ public class GoldenRamFleeceLayer extends LayerRenderer<GoldenRamEntity, GoldenR
     }
 
     public void render(MatrixStack stack, IRenderTypeBuffer buffer, int p_225628_3_, GoldenRamEntity entity, float p_225628_5_, float p_225628_6_, float p_225628_7_, float p_225628_8_, float p_225628_9_, float p_225628_10_) {
-        if (/*!p_225628_4_.isSheared() && */!entity.isInvisible()) {
+        if (!entity.isSheared() &&!entity.isInvisible()) {
             //IVertexBuilder vertexBuilder = buffer.getBuffer(RenderType.entityCutoutNoCull(FLEECE_TEXTURE));
             //model.renderToBuffer(stack, vertexBuilder, p_225628_3_, LivingRenderer.getOverlayCoords(entity, 0.0F), 1,1,1,1);
             coloredCutoutModelCopyLayerRender(this.getParentModel(), this.model, FLEECE_TEXTURE, stack, buffer, p_225628_3_, entity, p_225628_5_, p_225628_6_, p_225628_8_, p_225628_9_, p_225628_10_, p_225628_7_, 1,1,1);
