604a2f668690e21d275902fab9ed3aa3e83855f9
LocusAzzurro <dingzx1997@gmail.com>
2024-04-02 12:52:19 +0200

    :art: damage type datagen fix attempt

--- src/main/java/org/mineplugin/locusazzurro/icaruswings/datagen/ModDamageTagsGenerator.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/datagen/ModDamageTagsGenerator.java
@@ -3,20 +3,23 @@ package org.mineplugin.locusazzurro.icaruswings.datagen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.DamageTypeTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.mineplugin.locusazzurro.icaruswings.common.data.ModData;
 import org.mineplugin.locusazzurro.icaruswings.registry.DamageTypeRegistry;
 
 import javax.annotation.Nullable;
 import java.util.concurrent.CompletableFuture;
 
-public class ModDamageTagsGenerator extends TagsProvider<DamageType> {
+public class ModDamageTagsGenerator extends DamageTypeTagsProvider {
 
     public ModDamageTagsGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> provider, @Nullable ExistingFileHelper fh) {
-        super(output, Registries.DAMAGE_TYPE, provider, CompletableFuture.completedFuture(TagsProvider.TagLookup.empty()), DataGenerators.MOD_ID, fh);
+        super(output, provider, ModData.MOD_ID, fh);
     }
+
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         tag(DamageTypeTags.BYPASSES_ARMOR).add(DamageTypeRegistry.TIME_RIFT);
