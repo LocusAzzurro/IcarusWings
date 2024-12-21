6fc595f98bbc5861e121f259b25b3169f639d42a
LocusAzzurro <dingzx1997@gmail.com>
2024-12-22 01:28:08 +0800

    :bug: fix / by 0 in interdiction effect tick

--- src/main/java/org/mineplugin/locusazzurro/icaruswings/common/effect/InterdictionEffect.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/common/effect/InterdictionEffect.java
@@ -18,14 +18,14 @@ public class InterdictionEffect extends MobEffect {
     public boolean applyEffectTick(@Nullable LivingEntity livingEntity, int amplifier) {
         if (livingEntity instanceof Player player && player.isFallFlying()){
             player.stopFallFlying();
-            player.setDeltaMovement(player.getDeltaMovement().add(new Vec3(0.0, -0.5 * amplifier, 0.0)));
+            player.setDeltaMovement(player.getDeltaMovement().add(new Vec3(0.0, -0.5 * (amplifier + 1), 0.0)));
         }
         return true;
     }
 
     @Override
     public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
-        int interval = Math.max(1, 20 / amplifier);
+        int interval = Math.max(1, 20 / (amplifier + 1));
         return duration % interval == 0;
     }
 }
