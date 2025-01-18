ccc82dea8a50fb28291c1c6bddc12bd4afaca592
LocusAzzurro <dingzx1997@gmail.com>
2025-01-18 22:29:05 +0100

    :bento: block model datagen init

--- src/main/java/org/mineplugin/locusazzurro/icaruswings/datagen/ModBlockStateProvider.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/datagen/ModBlockStateProvider.java
@@ -1,8 +1,12 @@
 package org.mineplugin.locusazzurro.icaruswings.datagen;
 
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import org.mineplugin.locusazzurro.icaruswings.registry.BlockRegistry;
 
 public class ModBlockStateProvider extends BlockStateProvider {
 
@@ -12,5 +16,14 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
+        simpleBlock(BlockRegistry.BEESWAX_BLOCK);
+
+
     }
+
+
+    private void simpleBlock(DeferredHolder<Block, ? extends Block> block){
+        simpleBlockWithItem(block.get(), cubeAll(block.get()));
+    }
+
 }
