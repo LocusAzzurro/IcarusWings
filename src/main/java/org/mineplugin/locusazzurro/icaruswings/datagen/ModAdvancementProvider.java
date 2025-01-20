fa49a94c4568e922cffb9eecbb7917043d0bd86f
LocusAzzurro <dingzx1997@gmail.com>
2025-01-20 16:56:30 +0100

    :sparkles: rest of past advancements datagen

--- src/main/java/org/mineplugin/locusazzurro/icaruswings/datagen/ModAdvancementProvider.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/datagen/ModAdvancementProvider.java
@@ -1,17 +1,12 @@
 package org.mineplugin.locusazzurro.icaruswings.datagen;
 
-import com.google.common.collect.Streams;
-import it.unimi.dsi.fastutil.ints.IntList;
 import net.minecraft.advancements.*;
 import net.minecraft.advancements.critereon.*;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.inventory.SlotRange;
 import net.minecraft.world.inventory.SlotRanges;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -22,13 +17,12 @@ import net.neoforged.neoforge.common.data.AdvancementProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.mineplugin.locusazzurro.icaruswings.registry.ItemRegistry;
 
-import java.awt.image.ComponentSampleModel;
 import java.io.Serializable;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class ModAdvancementProvider extends AdvancementProvider {
 
@@ -71,6 +65,45 @@ public class ModAdvancementProvider extends AdvancementProvider {
                     .addCriterion("has_linen_undergarment", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.LINEN_UNDERGARMENT.get()))
                     .requirements(AdvancementRequirements.Strategy.OR)
                     .save(consumer, modLoc("past/linen_cloth").toString());
+            AdvancementHolder POTTERY = Advancement.Builder.advancement()
+                    .parent(PAST_ROOT)
+                    .display(makeDisplayInfo(ItemRegistry.MEAD_POT.get(), Category.PAST, "pottery", AdvancementType.TASK, true, true, false))
+                    .addCriterion("has_amphora", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.AMPHORA.get()))
+                    .addCriterion("has_mead_pot", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.MEAD_POT.get()))
+                    .requirements(AdvancementRequirements.Strategy.OR)
+                    .save(consumer, modLoc("past/pottery").toString());
+            AdvancementHolder BREW_MEAD = Advancement.Builder.advancement()
+                    .parent(POTTERY)
+                    .display(makeDisplayInfo(ItemRegistry.MEAD.get(), Category.PAST, "brew_mead", AdvancementType.TASK, true, true, false))
+                    .addCriterion("has_mead", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.MEAD.get()))
+                    .addCriterion("has_zephir_mead", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.ZEPHIR_INFUSED_MEAD.get()))
+                    .addCriterion("has_nether_mead", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.NETHER_INFUSED_MEAD.get()))
+                    .addCriterion("has_void_mead", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.VOID_INFUSED_MEAD.get()))
+                    .addCriterion("has_herbs_mead", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.HERBS_INFUSED_MEAD.get()))
+                    .addCriterion("has_golden_apple_mead", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.GOLDEN_APPLE_INFUSED_MEAD.get()))
+                    .requirements(AdvancementRequirements.Strategy.OR)
+                    .save(consumer, modLoc("past/brew_mead").toString());
+            AdvancementHolder PHILOSOPHER_STONE = Advancement.Builder.advancement()
+                    .parent(BREW_MEAD)
+                    .display(makeDisplayInfo(ItemRegistry.PHILOSOPHER_STONE.get(), Category.PAST, "philosopher_stone", AdvancementType.GOAL, true, true, false))
+                    .addCriterion("has_philosopher_stone", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.PHILOSOPHER_STONE.get()))
+                    .save(consumer, modLoc("past/philosopher_stone").toString());
+            AdvancementHolder WIND_WAND = Advancement.Builder.advancement()
+                    .parent(PHILOSOPHER_STONE)
+                    .display(makeDisplayInfo(ItemRegistry.WIND_WAND.get(), Category.PAST, "wind_wand", AdvancementType.CHALLENGE, true, true, false))
+                    .addCriterion("has_wind_wand", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.WIND_WAND.get()))
+                    .save(consumer, modLoc("past/wind_wand").toString());
+            AdvancementHolder ALL_COLORED_FEATHERS = Advancement.Builder.advancement()
+                    .parent(PAST_ROOT)
+                    .display(makeDisplayInfo(ItemRegistry.RED_FEATHER.get(), Category.PAST, "all_colored_feathers", AdvancementType.CHALLENGE, true, true, false))
+                    .addCriterion("has_red_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.RED_FEATHER.get()))
+                    .addCriterion("has_blue_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.BLUE_FEATHER.get()))
+                    .addCriterion("has_green_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.GREEN_FEATHER.get()))
+                    .addCriterion("has_cyan_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.CYAN_FEATHER.get()))
+                    .addCriterion("has_gray_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.GRAY_FEATHER.get()))
+                    .requirements(AdvancementRequirements.Strategy.AND)
+                    .rewards(AdvancementRewards.Builder.experience(50))
+                    .save(consumer, modLoc("past/all_colored_feathers").toString());
             AdvancementHolder WINGS_FALLING = Advancement.Builder.advancement()
                     .parent(ICARUS_FLIGHT)
                     .display(makeDisplayInfo(Items.PEONY, Category.PAST, "wings_falling", AdvancementType.CHALLENGE, true, true, true))
@@ -84,22 +117,24 @@ public class ModAdvancementProvider extends AdvancementProvider {
                             MinMaxBounds.Ints.atMost(0)))
                     .rewards(AdvancementRewards.Builder.experience(50))
                     .save(consumer, modLoc("past/wings_falling").toString());
+            AdvancementHolder WINGS_FALLING_ADV = Advancement.Builder.advancement()
+                    .parent(ICARUS_FLIGHT)
+                    .display(makeDisplayInfo(ItemRegistry.GOLDEN_FEATHER.get(), Category.PAST, "wings_falling_adv", AdvancementType.CHALLENGE, true, true, true))
+                    .addCriterion("broke_wings", ItemDurabilityTrigger.TriggerInstance.changedDurability(
+                            Optional.of(ContextAwarePredicate.create(
+                                    LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS,
+                                            EntityPredicate.Builder.entity().located(LocationPredicate.Builder.atYLocation(MinMaxBounds.Doubles.atLeast(2222f))).build()).build())),
+                            Optional.of(ItemPredicate.Builder.item().of(ItemRegistry.GOLDEN_FEATHER_WINGS.get()).build()),
+                            MinMaxBounds.Ints.atMost(0)))
+                    .rewards(AdvancementRewards.Builder.experience(100))
+                    .save(consumer, modLoc("past/wings_falling_adv").toString());
 
 
-
-            AdvancementHolder ALL_COLORED_FEATHERS = Advancement.Builder.advancement()
-                    .parent(PAST_ROOT)
-                    .display(makeDisplayInfo(ItemRegistry.RED_FEATHER.get(), Category.PAST, "all_colored_feathers", AdvancementType.CHALLENGE, true, true, false))
-                    .addCriterion("has_red_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.RED_FEATHER.get()))
-                    .addCriterion("has_blue_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.BLUE_FEATHER.get()))
-                    .addCriterion("has_green_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.GREEN_FEATHER.get()))
-                    .addCriterion("has_cyan_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.CYAN_FEATHER.get()))
-                    .addCriterion("has_gray_feather", InventoryChangeTrigger.TriggerInstance.hasItems(ItemRegistry.GRAY_FEATHER.get()))
-                    .requirements(AdvancementRequirements.Strategy.AND)
-                    .rewards(AdvancementRewards.Builder.experience(50))
-                    .save(consumer, modLoc("past/all_colored_feathers").toString());
 
         }
+
+
+
 
         private static DisplayInfo makeDisplayInfo(ItemLike display, Category category, String name, AdvancementType type, boolean showToast, boolean announceChat, boolean hidden){
             Optional<ResourceLocation> bg = name.equals(ROOT) ? Optional.of(ResourceLocation.fromNamespaceAndPath(DataGenerators.MOD_ID, "textures/gui/advancements/backgrounds/" + category + ".png")) : Optional.empty();
