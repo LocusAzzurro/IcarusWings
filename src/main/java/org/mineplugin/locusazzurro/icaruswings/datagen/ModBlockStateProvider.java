0b46c7b3d4835724b0b00c86a527671de1e5bf2e
LocusAzzurro <dingzx1997@gmail.com>
2025-01-21 00:18:19 +0100

    :art: elysian blocks and golden wool blocks model datagen

--- src/main/java/org/mineplugin/locusazzurro/icaruswings/datagen/ModBlockStateProvider.java
+++ src/main/java/org/mineplugin/locusazzurro/icaruswings/datagen/ModBlockStateProvider.java
@@ -1,13 +1,19 @@
 package org.mineplugin.locusazzurro.icaruswings.datagen;
 
+import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CakeBlock;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.client.model.generators.ConfiguredModel;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.mineplugin.locusazzurro.icaruswings.common.block.Amphora;
+import org.mineplugin.locusazzurro.icaruswings.common.block.MeadPot;
 import org.mineplugin.locusazzurro.icaruswings.registry.BlockRegistry;
 
 public class ModBlockStateProvider extends BlockStateProvider {
@@ -32,6 +38,30 @@ public class ModBlockStateProvider extends BlockStateProvider {
             return ConfiguredModel.builder().modelFile(
                     models().crop(name(BlockRegistry.FLAX_CROP.get()) + "_stage" + age, modLoc(BLOCK + name(BlockRegistry.FLAX_CROP.get()) + "_stage" + age)).renderType("cutout")).build();
         });
+        simpleBlock(BlockRegistry.ELYSIAN_GRASS_BLOCK.get(), ConfiguredModel.allYRotations(models().cubeBottomTop(
+                name(BlockRegistry.ELYSIAN_GRASS_BLOCK.get()), sideLoc(BlockRegistry.ELYSIAN_GRASS_BLOCK.get()),
+                blockLoc(BlockRegistry.ELYSIAN_SOIL.get()), topLoc(BlockRegistry.ELYSIAN_GRASS_BLOCK.get())
+                ), 0, false));
+        simpleBlock(BlockRegistry.ELYSIAN_SOIL.get());
+        simpleBlock(BlockRegistry.ELYSIAN_GRASS.get(), models().cross(name(BlockRegistry.ELYSIAN_GRASS.get()), blockLoc(BlockRegistry.ELYSIAN_GRASS.get())).renderType("cutout"));
+        simpleBlock(BlockRegistry.GOLDEN_WOOL_BLOCK.get());
+        simpleBlock(BlockRegistry.GOLDEN_WOOL_CARPET.get(), models().carpet(name(BlockRegistry.GOLDEN_WOOL_CARPET.get()), blockLoc(BlockRegistry.GOLDEN_WOOL_BLOCK.get())));
+        getVariantBuilder(BlockRegistry.HONEY_CAKE.get()).forAllStates(blockState -> {
+            int slice = blockState.getValue(CakeBlock.BITES);
+            return ConfiguredModel.builder().modelFile(models().getExistingFile(modLoc(BLOCK + name(BlockRegistry.HONEY_CAKE.get()) + "_slice" + slice))).build();
+        });
+        getVariantBuilder(BlockRegistry.MEAD_POT.get()).forAllStates(blockState -> {
+            var state = blockState.getValue(MeadPot.STATE);
+            return ConfiguredModel.builder().modelFile(models().getExistingFile(modLoc(BLOCK + name(BlockRegistry.MEAD_POT.get()) + "_" + state.getSerializedName()))).build();
+        });
+        getVariantBuilder(BlockRegistry.AMPHORA.get()).forAllStates(blockState -> {
+            DoubleBlockHalf half = blockState.getValue(Amphora.HALF);
+            Direction.Axis axis = blockState.getValue(Amphora.FACING).getAxis();
+            ModelFile.ExistingModelFile modelLower = models().getExistingFile(modLoc(BLOCK + "amphora_lower"));
+            ModelFile.ExistingModelFile modelUpper = models().getExistingFile(modLoc(BLOCK + "amphora_upper"));
+            return ConfiguredModel.builder().modelFile(half == DoubleBlockHalf.LOWER ? modelLower : modelUpper).rotationY(axis == Direction.Axis.X ? 0 : 90).build();
+        });
+
     }
 
     private void existingModelBlock(Block block){
@@ -48,6 +78,10 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     private ResourceLocation endLoc(Block block){
         return modLoc(BLOCK + name(block) + "_end");
+    }
+
+    private ResourceLocation topLoc(Block block){
+        return modLoc(BLOCK + name(block) + "_top");
     }
 
     private String name(Block block){
